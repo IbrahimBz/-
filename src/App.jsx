2a42e3915be32dd2246094a66534784b4cd43e45
Ibrahim <115651765+IbrahimBz@users.noreply.github.com>
2023-04-06 23:08:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "../tailwind.config";
+function App() {
+  return (
+    <div className="App">
+      <div className="text-center bg-">Ibrahim Is My Name</div>
+    </div>
+  );
+}
+
+export default App;
